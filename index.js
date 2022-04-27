6ca087afc92235749e781097a83fdbfa4700b5c9
Eric Tam <ronin619@Erics-MBP.fios-router.home>
2022-04-26 22:40:34 -0400

    feat: implement installation of express, pg, knex

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const port = process.env.PORT || 3000;
+const app = express();
+app.use(express.json());
+
+app.get("/hello", (req, res) => {
+    console.log("I am a console.log")
+    res.send("world");
+})
+
+app.listen(port, () => {
+    console.log(`Server running on port: ${port}`);
+});
