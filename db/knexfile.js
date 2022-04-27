21b6322a8994f456756b6c46a735ab8485d47746
Eric Tam <ronin619@Erics-MBP.fios-router.home>
2022-04-27 08:24:33 -0400

    feat: refactor knexfile and package.json

--- db/knexfile.js
+++ db/knexfile.js
@@ -1,4 +1,6 @@
-// Update with your config settings.
+require("dotenv").config({
+  path: "./.env.local",
+});
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
@@ -8,17 +10,16 @@ module.exports = {
   development: {
     client: 'postgresql',
     connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password'
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
     }
   },
-    pool: {
-      min: 2,
-      max: 10
-    },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds'
     }
   };
 
