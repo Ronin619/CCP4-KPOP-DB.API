923808dfb811c96f1ec82cb44352ac616f206ae5
Eric Tam <ronin619@Erics-MBP.fios-router.home>
2022-04-29 00:57:07 -0400

    feat: implement post and delete CRUD operation

--- server.js
+++ server.js
@@ -4,11 +4,26 @@ const port = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
 
-// TEST END POINT
-app.get('/test',  async (req, res) => {
-    const result = await db('groups').select('*');
-    res.send(result);
- })
+// CRUD OPERATIONS
+
+app.get('/groups',  async (req, res) => {
+     const result = await db('groups').select('*')
+     res.status(200).send(result);
+ });
+
+ app.post('/groups', async (req, res) => {
+    const body = req.body
+    await db('groups').insert(body)
+    const groups = await db('groups').select('*')
+    res.status(201).send(groups);
+});
+
+app.delete('/:group_name', async (req, res) => {
+    const body = req.body.group_name
+    await db('groups').where({group_name: body}).del()
+    res.status(201).end();
+})
+
 
 
 
